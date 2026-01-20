e15c9fa215f40a9c6780254298afe20819d3c160
Devanshi-Mahto <mahtodevanshi@gmail.com>
2026-01-20 23:35:00 +0530

    Added files of api of leave, warden

--- hostel_management/client/src/api/auth.api.js
+++ hostel_management/client/src/api/auth.api.js
@@ -7,11 +7,6 @@ export const loginUser = async (data) => {
 
 export const registerUser = async (data) => {
   const res = await axios.post("/auth/register", data);
-  return res.data;};
-
-  // Get logged-in user
-export const getMe = async () => {
-  const res = await axios.get("/auth/me");
   return res.data;
 };
 
